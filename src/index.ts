efd42f424a6d6f437e96f0d334a66c8028ec106e
Ígor José Rodrigues <igorjr2003@gmail.com>
2025-10-01 09:01:39 -0300

    feat: setup node project

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+function main() {
+  yargs()
+    .scriptName('gitusr')
+    .usage('$0 <cmd> [args]')
+    .command(
+      'hello [name]',
+      'welcome ter yargs!',
+      yargs => {
+        yargs.positional('name', {
+          type: 'string',
+          default: 'Cambi',
+          describe: 'the name to say hello to',
+        });
+      },
+      function (argv) {
+        console.log('hello', argv.name, 'welcome to yargs!');
+      }
+    )
+    .help()
+    .parse(hideBin(process.argv));
+}
+
+main();
